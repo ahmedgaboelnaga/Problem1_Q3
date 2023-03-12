cff7367276326f820a6a9142cc970808db2b7d88
ahmedgaboelnaga <ahmedgaboelnaga@gmail.com>
2023-03-12 21:17:24 +0200

    Add project files.

---
+++ Problem1_Q3/FileName.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	double length;
+
+	cout << "Enter the length in feet: ";
+	cin >> length;
+
+	cout << "The length in cm is: " << length * 30;
+}
